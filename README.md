59bfd47845db04759509cb35e5fbecc3a1982fda
zuzu <razym007@yandex.ru>
2022-04-11 23:04:35 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+шаблон для hadoop, почитай комменты во всех местах
+mvn clean install
+java -jar target/testTemplate-1.0-SNAPSHOT.jar
---
+++ src/test/java/ru/zuzu/WordCountTest.java
@@ -0,0 +1,12 @@
+package ru.zuzu;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class WordCountTest
+{
+    /**
+     * тут пишут тесты
+     */
+}
